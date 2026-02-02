72a413157a04eb2444cae8c4415ecdd0ed36c55b
an8kk <an8kk.sa@gmail.com>
2026-02-02 14:22:27 +0500

    Base64 auth

--- src/main/java/com/fintrack/backend/repository/UserRepository.java
+++ src/main/java/com/fintrack/backend/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.fintrack.backend.repository;
-
+import java.util.Optional;
 import com.fintrack.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
---
+++ src/main/java/com/fintrack/backend/service/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.fintrack.backend.service;
+
+import com.fintrack.backend.entity.User;
+import com.fintrack.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        // We use Email as the "Username" for login
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.emptyList() 
+        );
+    }
+}
