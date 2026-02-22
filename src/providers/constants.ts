7dc637c959bfb360d266461de1d1c53bc7d272b1
an8kk <an8kk.sa@gmail.com>
2026-02-22 20:00:22 +0500

    FIIIINNNNNAAAALLLLLL

--- src/providers/constants.ts
+++ src/providers/constants.ts
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:8080/api";
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api";
 export const TOKEN_KEY = "fintrack-admin-token";
 export const USER_KEY = "fintrack-admin-user";
